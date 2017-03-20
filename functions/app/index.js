40a9d2efe73c7a29b6f15e3b6680aab89140fb98
Kawin Glomjai <kawin.g@taskworld.com>
2017-03-20 14:29:39 +0700

    REST API

--- functions/app/index.js
+++ functions/app/index.js
@@ -1,14 +1,18 @@
 require('dotenv').config()
 const cors = require('cors')({ origin: true })
 const express = require('express')
+const bodyParser = require('body-parser')
 
-const auth = require('./auth')
+// const auth = require('./auth')
 const app = express()
 
 app.use(cors)
-app.use(auth)
-app.get('/', (req, res) => {
-  res.send(`Hello world ${req.token}`)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(auth)
+app.post('/', (req, res) => {
+  const body = req.body
+  res.status(200).json(body)
 })
 
 if (process.env.NODE_ENV === 'development') {
