2e56c902a6cff7c1161fda9ee0efc965db468e00
Kawin Glomjai <kawin.g@taskworld.com>
2017-03-25 11:44:50 +0700

    Modify bot

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ import Boom from 'boom'
 import Joi from 'joi'
 import crypto from 'crypto'
 import _ from 'lodash'
+import axios from 'axios'
 
 function signatureValidation (headers, body) {
   if (process.env.NODE_ENV === 'development') return true
@@ -22,17 +23,36 @@ function requestValidation (req) {
   return _.isEmpty(result.error)
 }
 
+async function reply (replyToken, messages) {
+  const replyUrl = 'https://api.line.me/v2/bot/message/reply'
+  const TOKEN = process.env.ACCESS_TOKEN
+  return await axios.post(
+    replyUrl,
+    {
+      replyToken: replyToken,
+      messages: messages
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${TOKEN}`,
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+}
+
 export default (req, res, next) => {
   if (!signatureValidation(req.headers, req.body)) throw Boom.unauthorized('invalid token')
   if (!requestValidation(req)) throw Boom.badRequest()
 
   const { events } = req.body
-  const result = _.map(events, (event) => {
+  const messages = _.map(events, (event) => {
     const { replyToken, type, source, message } = event
     const userId = source.userId
     const textType = message.type
     const text = message.text
+    return `fooo ${text}`
   })
-
+  console.log(messages)
   res.status(200).json('done')
 }
